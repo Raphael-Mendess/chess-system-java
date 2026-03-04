c9f18da42f47a9ffc716dca90422f00c5d2003da
Raphael-Mendess <raphael.hkiller@gmail.com>
2026-03-04 04:29:40 -0300

    Encoding all possible king moves on my chessboard.

--- src/application/UI.java
+++ src/application/UI.java
@@ -46,13 +46,13 @@ public class UI {
 
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + "  ");
-			for (int j = 0; j < pieces.length; j++) {
+			System.out.print((8 - i) + "   ");
+			for (int j = 0; j < pieces[i].length; j++) {
 				printPiece(pieces[i][j], false );
 			}
 			System.out.println();
 		}
-		System.out.println("   a  b  c  d  e  f  g  h");
+		System.out.println("    a  b  c  d  e  f  g  h");
 	}
 	
 	
@@ -60,14 +60,13 @@ public class UI {
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves ) {	
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + "   ");
-			for (int j = 0; j < pieces.length; j++) {
+			for (int j = 0; j < pieces[i].length; j++) { //corrigido "pieces[i]";
 				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("   a  b  c  d  e  f  g  h");
+		System.out.println("    a  b  c  d  e  f  g  h");
 	}
-
 
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if (background) {
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,11 +1,11 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
-
 	public King(Board board, Color color) {
 		super(board, color);
 	}
@@ -14,10 +14,66 @@ public class King extends ChessPiece {
 	public String toString() {
 		return "K";
 	}
-	
-	@Override
-	public boolean[][] possibleMoves(){
-		boolean [][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		return matriz;
+
+	private boolean canMove(Position position) {
+		ChessPiece pieceObject = (ChessPiece) getBoard().piece(position);
+		return pieceObject == null || pieceObject.getColor() != getColor();
+	}
+
+	@Override  //This was hard working kkkkkkkkkkk 
+	public boolean [][] possibleMoves(){
+		boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position positionOfPiece = new Position(0, 0);
+		
+		//above
+		positionOfPiece.setValues(position.getRow() - 1, position.getColumn());
+		if(getBoard().positionExists(positionOfPiece) && canMove(positionOfPiece)) {
+			matrix[positionOfPiece.getRow()] [positionOfPiece.getColumn()] = true;
+		}
+		
+		//below
+		positionOfPiece.setValues(position.getRow() + 1, position.getColumn());
+		if(getBoard().positionExists(positionOfPiece) && canMove(positionOfPiece)) {
+			matrix[positionOfPiece.getRow()] [positionOfPiece.getColumn()] = true;
+		}
+		
+		//left
+		positionOfPiece.setValues(position.getRow(), position.getColumn() - 1);
+		if(getBoard().positionExists(positionOfPiece) && canMove(positionOfPiece)) {
+			matrix[positionOfPiece.getRow()] [positionOfPiece.getColumn()] = true;
+		}
+
+		//right
+		positionOfPiece.setValues(position.getRow(), position.getColumn() + 1);
+		if(getBoard().positionExists(positionOfPiece) && canMove(positionOfPiece)) {
+			matrix[positionOfPiece.getRow()] [positionOfPiece.getColumn()] = true;
+		}
+		
+		//North(nw)
+		positionOfPiece.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if(getBoard().positionExists(positionOfPiece) && canMove(positionOfPiece)) {
+			matrix[positionOfPiece.getRow()] [positionOfPiece.getColumn()] = true;
+		}
+		
+		//North East(ne)
+		positionOfPiece.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if(getBoard().positionExists(positionOfPiece) && canMove(positionOfPiece)){
+			matrix[positionOfPiece.getRow()] [positionOfPiece.getColumn()] = true;
+		}
+		
+		//South West(sw)
+		positionOfPiece.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if(getBoard().positionExists(positionOfPiece) && canMove(positionOfPiece)){
+			matrix[positionOfPiece.getRow()] [positionOfPiece.getColumn()] = true;
+		}
+		
+		//South East(se)
+		positionOfPiece.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if(getBoard().positionExists(positionOfPiece) && canMove(positionOfPiece)){
+		  matrix[positionOfPiece.getRow()] [positionOfPiece.getColumn()] = true;
+		}
+		
+		return matrix;
 	}
 }
